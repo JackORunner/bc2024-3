cb32784b1a6eb5fc547c09ddf0aff6336b2df487
Bihun Yevhenii bc2024-3 <zhecarunner@gmail.com>
2024-10-01 16:20:32 +0300

    added new data file and in main.js added option for lowest rate

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
-const {program} = require('commander');
+const { program } = require('commander');
 const fs = require('fs');
 
 program
-    .requiredOption('-i, --input <path>', 'required option, path for file that would be readed')
-    .option('-o, --output <path>', ' path to the file with output')
-    .option('-d, --display', ' displays file into console')
-    .option('-ds, --displayrez', 'displays the task info')
+  .requiredOption('-i, --input <path>', 'required option, path for file that would be read')
+  .option('-o, --output <path>', 'path to the file with output')
+  .option('-d, --display', 'displays file into console')
+  .option('-ds, --displayrez', 'displays the maximum rate')
+  .option('-st, --stats', 'displays stats for file')
 
 program.parse(process.argv);
-
 const options = program.opts();
 
+// Перевірка наявності вхідного файлу
 if (!options.input) {
-    console.error('Please, specify input file');
-    process.exit(1);
-  };
+  console.error('Please, specify input file');
+  process.exit(1);
+}
 
+// Перевірка існування файлу
 if (!fs.existsSync(options.input)) {
-    console.error('Cannot find input file');
-    process.exit(1);
-  };
+  console.error('Cannot find input file');
+  process.exit(1);
+}
 
-if (!options.output && !options.display) {
-    process.exit(0);
-};
-
+// Читання файлу
 const data = fs.readFileSync(options.input, 'utf8');
-const parsedData = JSON.parse(data)
+const parsedData = JSON.parse(data);
 
-
+// Виведення вмісту файлу у консоль, якщо задано параметр -d
 if (options.display) {
   console.log('File content:\n', data);
 }
 
-if (options.output) {
-  fs.writeFileSync(options.output, data);
-  console.log(`File has been written to ${options.output}`);
-}
-
-let maxRate = 0; // Початкове значення максимального курсу
-
+// Знаходимо максимальний курс
+let maxRate = 0;
 for (let i = 0; i < parsedData.length; i++) {
   if (parsedData[i].rate > maxRate) {
-    maxRate = parsedData[i].rate; // Оновлюємо максимальний курс, якщо знайдено більше значення
+    maxRate = parsedData[i].rate;
   }
 }
 
-if (options.displayrez) {
-    console.log(`Максимальний курс: ${maxRate}`);
+let minRate = parsedData[0].rate;
+for (let i = 0; i > parsedData.length; i++) {
+  if (parsedData[i].rate < minRate) {
+    minRate = parsedData[i].rate;
   }
+}
+
+// Виведення результату в консоль, якщо задано параметр -ds
+if (options.displayrez) {
+  console.log(`Максимальний курс: ${maxRate}`);
+}
+
+// Записуємо результат у файл, якщо задано параметр -o
+if (options.output) {
+  const result = `Максимальний курс: ${maxRate}`;
+  fs.writeFileSync(options.output, result);
+  console.log(`Result has been written to ${options.output}`);
+}
+
+if (options.stats) {
+  console.log(`Максимальний курс: ${maxRate}`);
+  console.log(`Мінімальний курс: ${minRate}`);
+}
