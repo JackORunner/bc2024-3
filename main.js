37472266ee549901f402a97f0efab63d00249c73
Bihun Yevhenii bc2024-3 <zhecarunner@gmail.com>
2024-10-01 16:38:24 +0300

    fixed minrate loop

--- main.js
+++ main.js
@@ -11,28 +11,27 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
-// Перевірка наявності вхідного файлу
+
 if (!options.input) {
   console.error('Please, specify input file');
   process.exit(1);
 }
 
-// Перевірка існування файлу
+
 if (!fs.existsSync(options.input)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
 
-// Читання файлу
 const data = fs.readFileSync(options.input, 'utf8');
 const parsedData = JSON.parse(data);
 
-// Виведення вмісту файлу у консоль, якщо задано параметр -d
+
 if (options.display) {
   console.log('File content:\n', data);
 }
 
-// Знаходимо максимальний курс
+
 let maxRate = 0;
 for (let i = 0; i < parsedData.length; i++) {
   if (parsedData[i].rate > maxRate) {
@@ -40,19 +39,20 @@ for (let i = 0; i < parsedData.length; i++) {
   }
 }
 
-let minRate = parsedData[0].rate;
-for (let i = 0; i > parsedData.length; i++) {
+
+let minRate = Infinity; 
+for (let i = 0; i < parsedData.length; i++) { 
   if (parsedData[i].rate < minRate) {
     minRate = parsedData[i].rate;
   }
 }
 
-// Виведення результату в консоль, якщо задано параметр -ds
+
 if (options.displayrez) {
   console.log(`Максимальний курс: ${maxRate}`);
 }
 
-// Записуємо результат у файл, якщо задано параметр -o
+
 if (options.output) {
   const result = `Максимальний курс: ${maxRate}`;
   fs.writeFileSync(options.output, result);
